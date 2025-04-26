8b1b6f0fcd556a1f33a00aff7008e4f92d9bed06
Mohamed Reyad <mohamedreyadamer@gmail.com>
2025-04-26 23:00:35 +0300

    shopping Cart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,4 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
   ],
+  // {
+
+  // }
 };
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../core/services/cart.service';
+//import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +12,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  //busyservice = inject(BusyService);
+  cartservice = inject(CartService);
+}
